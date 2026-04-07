5d40c1a3a9e941fb31e8a051217f9d6dc7d57044
XC-907 <xitlallicastaneda2026@u.northwestern.edu>
2026-04-07 15:32:49 -0500

    RC Servo Sweep Implementation (CircuitPython)

---
+++ Homework2/circuitpythoncode.py
@@ -0,0 +1,32 @@
+# RC Servo sweep angle implementation (CircuitPython)
+import board     # get pin definitions
+import pwmio     # get PWM
+import time      # get sleep function
+
+# Create PWM Signal output on pin GP16
+servo = pwmio.PWMOut(board.GP16, variable_frequency=True)
+
+# Set Servo frequency
+servo.frequency = 50 # hz
+
+# Define minimum and maximum duty cycle
+min_duty = int(65535*(0.5/20))    # 2.5% duty cycle
+max_duty = int(65535*(2.5/20))    # 12.5% duty cycle
+
+# Define set servo function
+def set_servo_angle(angle):
+    duty = int(min_duty + (max_duty-min_duty)*(angle/180))
+    servo.duty_cycle = duty
+
+while True:
+    # Sweep forward (0 → 180)
+    for angle in range(0, 181, 1):
+        set_servo_angle(angle)
+        time.sleep(0.01)
+    
+    # Sweep backward (180 → 0)
+    for angle in range(181, 0, -1):
+        set_servo_angle(angle)
+        time.sleep(0.01)
+
+    time.sleep(0.5)
