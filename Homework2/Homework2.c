0ec158fea0e62700c237f87a3b6e5610894f9bdd
XC-907 <xitlallicastaneda2026@u.northwestern.edu>
2026-04-07 21:02:04 -0500

    Final Modifications to C & CircuitPython Code

--- Homework2/Homework2.c
+++ Homework2/Homework2.c
@@ -3,44 +3,47 @@
 #include "hardware/pwm.h" // CMakeLists.txt must have hardware_pwm in target_link_libraries
 #include "hardware/adc.h" // CMakeLists.txt must have hardware_adc in target_link_libraries
 
-#define PWMPIN 16
+void set_servo_angle(int angle);
 
-bool timer_interrupt_function(__unused struct repeating_timer *t) {
-    // read the adc
-    uint16_t result1 = adc_read();
-    // print the voltage
-    printf("%f\r\n",(float)result1/4095*3.3);
-    return true;
-}
+#define PWMPIN 16
 
 int main()
 {
-    stdio_init_all();  // Turns on usb and initializes variables
+    // turn on usb and initalize variables
+    stdio_init_all(); 
 
-    // turn on a timer interrupt
-    struct repeating_timer timer;
-    // -100 means call the function every 100ms
-    // +100 would mean call the function 100ms after the function has ended
-    add_repeating_timer_ms(-100, timer_interrupt_function, NULL, &timer);
-
-    // turn on the pwm, in this example to 10kHz with a resolution of 1500
+     // turn on the pwm 
     gpio_set_function(PWMPIN, GPIO_FUNC_PWM); // Set the Pin to be PWM
     uint slice_num = pwm_gpio_to_slice_num(PWMPIN); // Get PWM slice number
     // the clock frequency is 150MHz divided by a float from 1 to 255
-    float div = 10; // must be between 1-255
+    float div = 50; // must be between 1-255
     pwm_set_clkdiv(slice_num, div); // sets the clock speed
-    uint16_t wrap = 1500; // when to rollover, must be less than 65535
+    uint16_t wrap = 60000; // when to rollover, must be less than 65535
+    // set the PWM frequency and resolution
+    // this sets the PWM frequency to 150MHz/div/wrap
     pwm_set_wrap(slice_num, wrap); 
     pwm_set_enabled(slice_num, true); // turn on the PWM
 
-    pwm_set_gpio_level(PWMPIN, 1500/2); // set the duty cycle to 50%
-
-    // turn on the adc
-    adc_init();
-    adc_gpio_init(26); // pin GP26 is pin ADC0
-    adc_select_input(0); // sample from ADC0
-
     while (true) {
-        tight_loop_conents(); // do nothing here, the interrupt does the work
+
+        //Sweep forward (0 → 180)
+        for (int angle=0; angle <= 180; angle++)
+        {
+            set_servo_angle(angle);
+            sleep_ms(10);
+        }
+        
+        // Sweep backward (180 → 0)
+        for (int angle=180; angle >= 0; angle--)
+        {
+            set_servo_angle(angle);
+            sleep_ms(10);
+        }
+
     }
+
+}
+
+void set_servo_angle(int angle){
+    pwm_set_gpio_level(PWMPIN, (int)((0.02 + (angle/180.0)*0.1)*60000));
 }
--- Homework2/circuitpythoncode.py
+++ Homework2/circuitpythoncode.py
@@ -10,8 +10,8 @@ servo = pwmio.PWMOut(board.GP16, variable_frequency=True)
 servo.frequency = 50 # hz
 
 # Define minimum and maximum duty cycle
-min_duty = int(65535*(0.5/20))    # 2.5% duty cycle
-max_duty = int(65535*(2.5/20))    # 12.5% duty cycle
+min_duty = int(65535*(0.4/20))    # 2% duty cycle
+max_duty = int(65535*(2.4/20))    # 12% duty cycle
 
 # Define set servo function
 def set_servo_angle(angle):
@@ -28,5 +28,3 @@ while True:
     for angle in range(181, 0, -1):
         set_servo_angle(angle)
         time.sleep(0.01)
-
-    time.sleep(0.5)
